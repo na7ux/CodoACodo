4129e03a7189e402e8dbcfb61d2e36b57b9e7571
na7ux <ll-.-n47ux-.-ll@hotmail.com>
2024-07-06 19:52:18 -0300

    backend - curd

---
+++ entorno_virtual/app/views.py
@@ -0,0 +1,50 @@
+from flask import jsonify, request
+from app.models import User
+
+def index():
+    return jsonify({'message':'heenlo'})
+
+def create_user():
+    data = request.json
+    new_user = User(
+        user_name=data['name'], 
+        user_last_name=data['last_name'], 
+        email=data['email'], 
+        phone=data['phone'],
+        user_usuario=data['user'],
+        user_password=data['password'],
+        image=data['image'])
+    new_user.save()
+    return jsonify({'message': 'User created successfully'}), 201
+
+def get_all_users():
+    users = User.get_all()
+    return jsonify([users.serialize() for users in users])
+
+def get_user(user_id):
+    user = User.get_by_id(user_id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify(user.serialize())
+
+def update_user(user_id):
+    user = User.get_by_id(user_id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    data = request.json
+    user.user_name = data['name']
+    user.user_last_name = data['last_name']
+    user.email = data['email']
+    user.phone = data['phone']
+    user.user_usuario = data['user']
+    user.user_password = data['password']
+    user.image = data['image']
+    user.save()
+    return jsonify({'message': 'User updated successfully'})
+
+def delete_user(user_id):
+    user = User.get_by_id(user_id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    user.delete()
+    return jsonify({'message': 'User deleted successfully'})
---
+++ entorno_virtual/run.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_cors import CORS
+from app.database import init_app
+from app.views import *
+
+app= Flask(__name__)
+
+# Configurar la aplicación Flask
+# app.config.from_pyfile('config/development.py')
+
+# Inicializar la base de datos con la aplicación Flask
+init_app(app)
+#permitir solicitudes desde cualquier origen
+CORS(app)
+#permitir solicitudes desde un origen específico
+# CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:5000"}})
+
+# Rutas para el CRUD de la entidad Users
+app.route('/', methods=['GET'])(index)
+app.route('/api/users/', methods=['POST'])(create_user)
+app.route('/api/users/', methods=['GET'])(get_all_users)
+app.route('/api/users/<int:user_id>', methods=['GET'])(get_user)
+app.route('/api/users/<int:user_id>', methods=['PUT'])(update_user)
+app.route('/api/users/<int:user_id>', methods=['DELETE'])(delete_user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
