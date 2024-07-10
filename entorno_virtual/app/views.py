a991f00a4a40c6022602973fff5971e07b1776cb
Gomez Juan <gomezjuan_jmag@hotmail.com>
2024-07-10 11:19:37 -0300

    corrección bd
    cambios varios

--- entorno_virtual/app/views.py
+++ entorno_virtual/app/views.py
@@ -1,31 +1,32 @@
-from flask import jsonify, request,render_template
+from flask import jsonify, request, render_template
 from app.models import User
-
-
 
 
 def create_user():
     data = request.json
     new_user = User(
-        user_name=data['name'], 
-        user_last_name=data['last_name'], 
-        email=data['email'], 
+        user_name=data['name'],
+        user_last_name=data['last_name'],
+        email=data['email'],
         phone=data['phone'],
         user_usuario=data['user'],
         user_password=data['password'],
         image=data['image'])
     new_user.save()
-    return jsonify({'message': 'User created successfully'}),201
+    return jsonify({'message': 'User created successfully'}), 201
+
 
 def get_all_users():
     users = User.get_all()
     return jsonify([users.serialize() for users in users])
+
 
 def get_user(user_id):
     user = User.get_by_id(user_id)
     if not user:
         return jsonify({'message': 'User not found'}), 404
     return jsonify(user.serialize())
+
 
 def update_user(user_id):
     user = User.get_by_id(user_id)
@@ -41,6 +42,7 @@ def update_user(user_id):
     user.image = data['image']
     user.save()
     return jsonify({'message': 'User updated successfully'})
+
 
 def delete_user(user_id):
     user = User.get_by_id(user_id)
--- entorno_virtual/run.py
+++ entorno_virtual/run.py
@@ -3,27 +3,31 @@ from flask_cors import CORS
 from app.database import init_app
 from app.views import *
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 # Configurar la aplicación Flask
 # app.config.from_pyfile('config/development.py')
 
 # Inicializar la base de datos con la aplicación Flask
 init_app(app)
-#permitir solicitudes desde cualquier origen
+# permitir solicitudes desde cualquier origen
 CORS(app)
-#permitir solicitudes desde un origen específico
+# permitir solicitudes desde un origen específico
 # CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:5000"}})
 
 # Rutas para el CRUD de la entidad Users
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
 """ @app.route('/registro')
 def registro():
     return render_template('registro.html') """
 
-app.route('/registro1', methods=['POST'])(create_user)
+app.route('/registro', methods=['POST'])(create_user)
 app.route('/registro', methods=['GET'])(get_all_users)
 app.route('/api/users/<int:user_user>', methods=['GET'])(get_user)
 app.route('/api/users/<string:user_user>', methods=['PUT'])(update_user)
